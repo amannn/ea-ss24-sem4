8d64c8a6827c1fbb0c79603797ce16d619237b07
Jan Amann <jan@amann.me>
2024-03-27 12:03:16 +0100

    Synchronizing mouse events into state
    To get started, we'll implement a custom hook that returns a boolean, indicating whether the mouse is currently "down". We're using this boolean to render emojis, indicating if sound is currently playing (i.e. the party has started) or not.
    
    To implement the custom hook, we'll register a global event listener for `mousedown` and `mouseup` and update an internal state accordingly.
    
    Note that we can move this hook to a separate file, but we'll leave it here for now for the sake of simplicity of this example.

--- 04-managing-state/src/Theremin.tsx
+++ 04-managing-state/src/Theremin.tsx
@@ -1,3 +1,45 @@
+import {useEffect, useState} from 'react';
+
 export default function Theremin() {
-  return <p>Theremin</p>;
+  const isActive = useMouseDown();
+
+  return (
+    <p
+      style={{
+        fontSize: 100,
+        textAlign: 'center',
+        userSelect: 'none'
+      }}
+    >
+      {isActive ? '💃🏻🕺🪩' : '🙄'}
+    </p>
+  );
+}
+
+function useMouseDown() {
+  const [isMouseDown, setIsMouseDown] = useState(false);
+
+  useEffect(() => {
+    function onMouseDown() {
+      setIsMouseDown(true);
+    }
+
+    function onMouseUp() {
+      setIsMouseDown(false);
+    }
+
+    window.addEventListener('mousedown', onMouseDown);
+    window.addEventListener('mouseup', onMouseUp);
+
+    // Remember to always clean up event listeners
+    // if you register them in an effect
+    return () => {
+      window.removeEventListener('mousedown', onMouseDown);
+      window.removeEventListener('mouseup', onMouseUp);
+    };
+  }, []);
+  // An empty array means this effect will only run
+  // once, after the first render of this component.
+
+  return isMouseDown;
 }
